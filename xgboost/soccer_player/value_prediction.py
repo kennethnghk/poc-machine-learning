90be497a0a67883ec502ac8433d198cf0da6af1a
Kenneth Ng <ng.tszcheong@gmail.com>
2019-01-31 09:17:59 +0800

    Add plot to single X column

--- xgboost/soccer_player/value_prediction.py
+++ xgboost/soccer_player/value_prediction.py
@@ -2,17 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def loadDataset(file):
+def load_dataset(file):
     df = pd.read_csv(filepath_or_buffer=file)
     return df
 
-def featureSet(df):
-    selectedFeatures = df[['club','league', 'potential', 'international_reputation']]
+def feature_set(df):
+    selected_features = df[['club','league', 'potential', 'international_reputation']]
     # print (selectedFeatures)
-    X_list = selectedFeatures.values.tolist()
+    X_list = selected_features.values.tolist()
     y_list = df.y.values
     return X_list, y_list
 
 
-trainData = loadDataset('dataset/train.csv')
-X_train, y_train = featureSet(trainData); 
+train_data = load_dataset('dataset/train.csv')
+X_train, y_train = feature_set(train_data); 
+
+X_train_arr = np.array(X_train)
+X_plot = X_train_arr[:,0] ## select 1st column to plot
+
+plt.figure()
+plt.scatter(X_plot, y_train, s=20, edgecolor="black", c="darkorange", label="data")
+plt.xlabel("club")
+plt.ylabel("target")
+plt.title("Train data")
+plt.legend()
+plt.show()
+
